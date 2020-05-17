cdf3ac5d5f4a6ae8a4b3f07e531b2a56e97834a2
Jean-Marc Jones <jean-marc@care-planner.co.uk>
2020-05-17 23:04:31 +0100

    Get site title from config

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,16 +1,29 @@
 import React, { FunctionComponent } from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
 import * as styles from './layout.module.css'
 import ListLink from './ListLink'
 
 const Layout: FunctionComponent = ({ children }) => {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+
   return (
     <div className={styles.layout}>
       <header className={styles.header}>
-        <h3>Broken Core</h3>
+        <h3>{data.site.siteMetadata.title}</h3>
         <ul className={styles.header__nav}>
-          <ListLink to='/'>Home</ListLink>
-          <ListLink to='/artists/'>Artists</ListLink>
-          <ListLink to='/social/'>Social</ListLink>
+          <ListLink to="/">Home</ListLink>
+          <ListLink to="/artists/">Artists</ListLink>
+          <ListLink to="/social/">Social</ListLink>
         </ul>
       </header>
       {children}
