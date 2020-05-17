4443bd8e11d2a9f4aedb35f621037d0850a013d6
Jean-Marc Jones <jean-marc@care-planner.co.uk>
2020-05-17 21:45:22 +0100

    Add header

--- src/components/layout.module.css.d.ts
+++ src/components/layout.module.css.d.ts
@@ -1,1 +1,2 @@
-export const container: string;
+export const layout: string;
+export const header: string;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,7 +2,14 @@ import React, { FunctionComponent } from 'react'
 import * as styles from './layout.module.css'
 
 const Layout: FunctionComponent = ({ children }) => {
-  return <div className={styles.container}>{children}</div>
+  return (
+    <div className={styles.layout}>
+      <header className={styles.header}>
+        <h3>Broken Core</h3>
+      </header>
+      {children}
+    </div>
+  )
 }
 
 export default Layout
