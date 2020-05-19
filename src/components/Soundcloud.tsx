b97cc3dcc903b321a1779d88dfa32583768a59e5
Jean-Marc Jones <jean-marc@care-planner.co.uk>
2020-05-19 19:24:31 +0100

    Soundcloud widget component

---
+++ src/components/Soundcloud.tsx
@@ -0,0 +1,20 @@
+import React, { FunctionComponent } from 'react'
+
+interface Props {
+  id: string
+}
+
+const Soundcloud: FunctionComponent<Props> = ({ id }) => {
+  return (
+    <iframe
+      width="100%"
+      height="300"
+      scrolling="no"
+      frameBorder="no"
+      allow="autoplay"
+      src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/${id}&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true`}
+    />
+  )
+}
+
+export default Soundcloud
