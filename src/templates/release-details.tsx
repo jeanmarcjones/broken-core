c94ef2775b178fc069b3ce4a78cfbdd39fbd01e8
Jean-Marc Jones <jean-marc@care-planner.co.uk>
2020-05-22 08:02:41 +0100

    Generate release pages from templates

---
+++ src/templates/release-details.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from 'react'
+import Layout from '../components/layout'
+
+const ReleaseDetails: FunctionComponent = () => {
+  return (
+    <Layout>
+      <h2>hello release</h2>
+    </Layout>
+  )
+}
+
+export default ReleaseDetails
