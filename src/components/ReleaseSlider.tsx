343448bd577d6cdeef936f6ae7d03551ba622999
Jean-Marc Jones <jean-marc@care-planner.co.uk>
2020-10-23 18:17:05 +0100

    First pass at react spring widget

--- src/components/ReleaseSlider.tsx
+++ src/components/ReleaseSlider.tsx
@@ -1,7 +1,11 @@
-import React, { FunctionComponent, useMemo, useState } from 'react'
+import React, {
+  FunctionComponent,
+  useState,
+  useRef,
+  useEffect,
+} from 'react'
 import { graphql, useStaticQuery, Link } from 'gatsby'
-import { CSSTransition } from 'react-transition-group'
-import Soundcloud from './Soundcloud'
+import { animated, useTransition } from 'react-spring'
 import { ReleaseNode } from '../types/graphql/ReleaseNode'
 import * as styles from './ReleaseSlider.module.css'
 
@@ -16,9 +20,6 @@ interface Data {
 }
 
 const ReleaseSlider: FunctionComponent = () => {
-  const [currentPage, setCurrentPage] = useState<number>(0)
-  const [state, setState] = useState<boolean>(true)
-
   const data: Data = useStaticQuery(
     graphql`
       query {
@@ -39,52 +40,61 @@ const ReleaseSlider: FunctionComponent = () => {
     `
   )
 
+  const [width, setWidth] = useState<number>(0)
+  const parentRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    if (parentRef.current) setWidth(parentRef.current.offsetWidth)
+  }, [parentRef])
+
   const releases = data.allMarkdownRemark.edges.sort(
     (a, b) => b.node.frontmatter.cat - a.node.frontmatter.cat
   )
   const maxPage = releases.length - 1
 
-  const toggleState = () => setState(state => !state)
+  const [reverse, setReverse] = useState<boolean>(false)
+  const [currentPage, setCurrentPage] = useState<number>(0)
+  const transitions = useTransition(currentPage, item => item, {
+    from: {
+      position: 'absolute',
+      transform: 'translate3d(-150vh,0,0)',
+    },
+    enter: { transform: 'translate3d(0vh,0,0)' },
+    leave: { transform: 'translate3d(+150vh,0,0)' },
+    reverse,
+  })
 
   const next = () => {
-    setState(false)
+    setReverse(false)
     const page = currentPage === maxPage ? 0 : currentPage + 1
     setCurrentPage(page)
-    // toggleState()
-    setState(true)
   }
 
   const prev = () => {
-    setState(false)
+    setReverse(true)
     const page = currentPage === 0 ? maxPage : currentPage - 1
     setCurrentPage(page)
-    // toggleState()
-    setState(true)
   }
 
   return (
-    <div style={{ flex: 1 }}>
-      <CSSTransition
-        in={state}
-        timeout={900}
-        classNames="fade"
-        unmountOnExit
-        // onEnter={() => setState(false)}
-        // onExited={() => setState(false)}
-      >
-        <Soundcloud
-          id={releases[currentPage].node.frontmatter.soundcloud}
-          key={currentPage}
-        />
-      </CSSTransition>
+    <div className={styles.releaseSlider} ref={parentRef}>
+      {transitions.map(({ item, key, props }) => {
+        return (
+          <animated.iframe
+            key={key}
+            style={props}
+            width={width}
+            height="300"
+            scrolling="no"
+            frameBorder="no"
+            src={`https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/${releases[item].node.frontmatter.soundcloud}&color=%23ff5500&auto_play=false&hide_related=true&show_comments=false&show_user=true&show_reposts=false&show_teaser=true&visual=true`}
+          />
+        )
+      })}
       <div className={styles.releaseSlider__buttonWrapper}>
         <button className={styles.releaseSlider__button} onClick={prev}>
           prev
         </button>
-        <Link
-          className={styles.releaseSlider__button}
-          to={releases[currentPage].node.fields.slug}
-        >
+        <Link className={styles.releaseSlider__button} to="/">
           details
         </Link>
         <button className={styles.releaseSlider__button} onClick={next}>
